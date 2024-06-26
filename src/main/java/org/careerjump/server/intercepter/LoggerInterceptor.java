b6aa6b4d5f6500663b0adee22d5c0cf150b5b91a
Dae-Jeong <v4chelsea@gmail.com>
2024-06-26 14:51:40 +0900

    FEAT : Log Intercepter 추가

--- src/main/java/org/careerjump/server/intercepter/LoggerInterceptor.java
+++ src/main/java/org/careerjump/server/intercepter/LoggerInterceptor.java
@@ -3,10 +3,15 @@ package org.careerjump.server.intercepter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+import static org.careerjump.server.constant.LogConstant.TRACING_ID;
 
 @Slf4j
 @Component
@@ -14,20 +19,30 @@ public class LoggerInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
 
-        if (log.isDebugEnabled()) {
-            log.debug("===================       START       ===================");
-            log.debug("Request time: {}, IP: {}", new java.util.Date(), request.getRemoteAddr());
-            log.debug("Request {} {}", request.getMethod(), request.getRequestURI());
-        }
+        String uuid = UUID.randomUUID().toString();
+        MDC.put(TRACING_ID, uuid);
+
+        log.info("REQUEST [{}][{}][{}]", request.getDispatcherType(), requestURI, handler);
 
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        if (log.isDebugEnabled()) {
-            log.debug("===================        END        ===================");
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {}
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = MDC.get(TRACING_ID);
+
+        log.info("RESPONSE [{}][{}]", request.getDispatcherType(), requestURI);
+
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
         }
+
+        MDC.clear();
     }
 }
